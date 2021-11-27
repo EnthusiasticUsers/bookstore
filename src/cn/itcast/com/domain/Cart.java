c478e0ed21a9848da33856af6332b4bab6001a41
passionate <2335275255@qq.com>
2021-11-27 11:28:33 +0800

    '书店1.3'

---
+++ src/cn/itcast/com/domain/Cart.java
@@ -0,0 +1,67 @@
+package cn.itcast.com.domain;
+
+public class Cart {
+    private Integer id;
+    private String name;
+    private Double price;
+    private String image;
+    private String type;
+    private Integer number;
+    private Integer uid;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+}
--- src/cn/itcast/com/servlet/CartServlet.java
+++ src/cn/itcast/com/servlet/CartServlet.java
@@ -12,6 +12,7 @@ public class CartServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("utf-8");
         response.setCharacterEncoding("utf-8");
+
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
